664e81df6d6d1af21ae13ab9814a1fc3a5c87dbe
Abdul Alkout <Abdul.Alkout@gmail.com>
2021-05-24 20:17:27 +0200

    new table  title in db

--- job/models.py
+++ job/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+'''
+django modle field
+    - html widget
+    - validation 
+    - db size 
+
+'''
+class Job (models.Model) : # table
+    title = models.CharField(max_length=100)  #coulmn
